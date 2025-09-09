43f4db9ca897f03326951c4bd48c0714e8e8973a
Sander van Gemmert <sandervangemmert@gmail.com>
2025-09-10 00:10:43 +0200

    first setup, working on camera

---
+++ main.gd
@@ -0,0 +1,20 @@
+extends Node
+## enums
+## consts
+## exports
+## public vars
+## private vars
+## onread vars
+## built-in override methods
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+## public methods
+
+## private methods
---
+++ script_templates/node/basic_script.gd
@@ -0,0 +1,25 @@
+# meta-name: basic script
+# meta-description: basic script with structures
+# meta-default: true
+# meta-space-indent: 4
+extends Node
+## enums
+## consts
+## exports
+## public vars
+## private vars
+## onready vars
+
+## built-in override methods
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+## public methods
+
+## private methods
